d4068d107c07f2ebe70307e723d27cd70544686c
mmaaddiieemm <madgmeier@gmail.com>
2020-02-08 12:48:22 -0600

    basic input validation, but will need to handle how to convert values to strings

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, {useState} from 'react';
-import {InputGroup} from "react-bootstrap";
+import {InputGroup, FormControl, Button} from "react-bootstrap";
 import DisplayResults from "./DisplayResults";
 import './App.css';
 
 function App() {
-  // const [searchCriteria, setSearchCriteria] = useState(""); 
+  const [searchCriteria, setSearchCriteria] = useState(""); 
 
-  const searchKey = "id"; 
-  const value = 2; 
+  var searchKey, value = ""; 
+  if (searchCriteria.split(":").length > 1) {
+    searchKey=searchCriteria.split(":")[0]; 
+    value=searchCriteria.split(":")[1]; 
+  }
   return (
-    <div className="App">
+    <div className="App-header">
       <p>What do you want to search? </p>
-      <p>available options: -</p>
-      <InputGroup></InputGroup>
+      <p>(Your query must be in the format 'search term':'value')</p>
+      <InputGroup className="mb-3">
+        <InputGroup.Prepend>
+          {/* <Button variant="outline-secondary"></Button> */}
+        </InputGroup.Prepend>
+        <FormControl aria-describedby="basic-addon1" onChange={e => setSearchCriteria(e.target.value)}/>
+      </InputGroup>
       <p>Results: </p>
       <DisplayResults searchKey={searchKey} value={value}></DisplayResults>
     </div>
