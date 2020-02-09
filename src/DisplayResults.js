a6ebd96749e130db82acb4d5ccbc8500af6c9356
mmaaddiieemm <madgmeier@gmail.com>
2020-02-09 17:44:16 -0600

    improved data formatting, handle undefined/null data

--- src/DisplayResults.js
+++ src/DisplayResults.js
@@ -7,19 +7,20 @@ import Organizations from './Organizations.js';
 const OrgDb = require('./data/organizations.json');
 const TicketsDb = require('./data/tickets.json');
 const UsersDb = require('./data/users.json');
+ 
 
 function DisplayResults({ searchKey, value }) {
   const users = Search(searchKey, value, UsersDb);
   const tickets = Search(searchKey, value, TicketsDb);
   const orgs = Search(searchKey, value, OrgDb);
-
+  
   if (users.length === 0 && tickets.length === 0 && orgs.length === 0 ) {
-    return <p>No entry found that matches criteria</p>; 
+    return <p className="App-results">No entry found that matches criteria</p>; 
   } else {
     return (
       <div>
-        {users.map((i) => <Users key={i.external_id} userData={i} />)}
-        {tickets.map((j) => <Tickets key={j.external_id} ticketData={j} />)}
+        <Users users={users} />
+        <Tickets tickets={tickets} />
         {orgs.map((k) => <Organizations key={k.external_id} orgData={k} />)}
       </div>
     ); 
