6df682c1644b705facbe11e51aabbc36cf0b58f1
Maddie Meier <madgmeier@gmail.com>
2020-02-11 19:19:35 -0600

    i think basic input validation should be working okay now

--- src/App.js
+++ src/App.js
@@ -3,28 +3,39 @@ import { InputGroup, FormControl } from 'react-bootstrap';
 import CollectResults from './CollectResults';
 import './App.css';
 
+
 function App() {
   const [searchCriteria, setSearchCriteria] = useState('');
 
+  // parse the input data. 
   let searchKey = searchCriteria;
-  let dataTypes = 'uto'; 
+  let dataTypes = 'uto'; // search all types by default
   let value;
+
   if (searchKey.split(" ").length > 1) {
-    dataTypes = searchKey.split(" ")[0].split('-')[1]; 
+    dataTypes = searchKey.split(" ")[0]; 
     console.log(dataTypes); 
-    searchKey = searchKey.split(" ")[1]; 
+    searchKey = searchKey.slice(searchKey.indexOf(" ")).trim(); 
+    console.log(searchKey); 
   }
   let parsedArr = searchKey.split('=');
   if (parsedArr.length > 1) {
     searchKey = parsedArr[0];
     value = (parsedArr[1] === null) ? '' : parsedArr[1];
+    console.log(searchKey); 
+    console.log(value); 
   } else {
     value = '';
   }
+
+
   return (
     <div className="App-header">
       <p>What do you want to search? </p>
-      <p className="App-instructions">Usage: [datatype] &lt;criteria&gt;=&lt;value&gt;</p>
+      <p className="App-instructions">Usage: [datatype] &lt;criteria&gt;=&lt;value&gt;<br/>
+          To search for empty values, use the value 'undefined'<br/>
+          All inputs are currently case sensitive.<br/>
+      </p>
       <InputGroup className="mb-3">
         <FormControl aria-describedby="basic-addon1" onChange={(e) => setSearchCriteria(e.target.value)} />
       </InputGroup>
--- src/CollectResults.js
+++ src/CollectResults.js
@@ -11,22 +11,10 @@ const OrgDb = require('./data/organizations.json');
 function CollectResults({ searchKey, value, dataTypes }) {
   // search for all matching key:value pairs in the data and add them 
   // to the results. 
+
   var users = (dataTypes.includes('u')) ? Search(searchKey, value, UsersDb) : [];
   var tickets = (dataTypes.includes('t')) ? Search(searchKey, value, TicketsDb) : [];
   var orgs = (dataTypes.includes('o')) ? Search(searchKey, value, OrgDb) : [];
-
-  // this block attempts to identify the related items and adds them to the list. 
-  // this definitely needs to be updated to utilize some sort of set so that 
-  // we don't get duplication of entries. 
-  // if ( typeof searchKey !== 'undefined' && typeof value !== 'undefined' && !searchKey.includes('id')) {
-  //   // loop through secondary results and add them to the list? 
-  //   let firstResults = users.concat(tickets).concat(orgs); 
-  //   for (var result in firstResults) {
-  //     users = users.concat(Search('id', firstResults[result]._id, UsersDb)); 
-  //     tickets = tickets.concat(Search('id', firstResults[result]._id, TicketsDb)); 
-  //     orgs = orgs.concat(Search('id', firstResults[result]._id, OrgDb)); 
-  //   }    
-  // }
   
   if (users.length === 0 && tickets.length === 0 && orgs.length === 0 ) {
     return <p className="App-results">No entry found that matches criteria</p>; 
